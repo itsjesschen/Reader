d2e29f529e161d7d787ae2db40f827df38d64992
Jessica Chen <itsjesschen@gmail.com>
2013-02-24 17:41:19 -0800

    updating css

--- public/js/content_display.js
+++ public/js/content_display.js
@@ -2,91 +2,104 @@
 window.onload = populateInitialFeeds;
 
 function populateInitialFeeds(){
-	// populateInitialTweets();
 	populateInitialRSS();
 	initDescription();
 }
 
-function deleteLinks(){
-
+function focusedText(item) {
+    item.style.color = "#333";
+}
+            
+function blurText(item) {
+    item.style.color = "#888";
+    if(item.value == ""){
+        item.value = item.defaultValue;
+    }
+}
+function searchFieldDisplay(item){
+    if(item.value == item.defaultValue ){//} || item.value =="search for"){
+        item.value = "";
+    }
 }
 
 function initDescription(){ // so that only 1 click handler is assigned
 	var $min = $("ul.article-list");
-	$min.on('click','div', function(){
+	$min.on('click','div.art-desc', function(){
     			$this = $(this);
     			var $description = $this.find("p");
-    			if( $description.hasClass("expand")){
-    				$description.removeClass("expand");
+    			if( $this.hasClass("expand")){
+    				$this.parent().removeClass("expand");
     				$this.removeClass("expand");
-    				// $description.slideUp();
     			}else{
-    				$description.addClass("expand");
+    				$this.parent().addClass("expand");
     				$this.animate($this.addClass("expand"), 1000, null);
-    				// $description.slideDown();
     			}
     });
     $( "#RSSModule" ).sortable();
     $( "#RSSModule" ).disableSelection();
 }
 function getRSS(Feed){
-	var URL = "../showfeed/showRSS?URL=" + Feed.id;
+	var URL = "../public/showfeed/showRSS?URL=" + Feed.id;
 	var $min = $("ul.article-list");
 
-	var xhr = new XMLHttpRequest();
-	xhr.open('GET', URL, true);
-	xhr.onreadystatechange = function(){
-		if(xhr.readyState === 4 && xhr.status === 200){
-			var xml = xhr.responseText;
-			// console.log(xml);
-			var $xmlitems = $(xml).find("item");
+	$.ajax({
+		type:"GET",
+	    url: "../public/showfeed/showRSS",
+	    data:{
+	    	URL : ""+Feed.id
+	    },
+	  	dataType: "xml",
+	    success: function(data){
+	    	var xml;
+	    	if (typeof data == "string") {
+	    		xml = new ActiveXObject("Microsoft.XMLDOM");
+	        	xml.async = false;
+	       		xml.loadXML(data);
 
-    		$xmlitems.each(function() {
-	         var $this = $(this),
-	            item = {
-	                title: $this.find("title").text(),
-	                link: $this.find("link").text(),
-	                description: $this.find("description").text(),
-	                pubDate: $this.find("pubDate").text(),
-	                author: $this.find("author").text()
-	        	}
-	        	console.log(this);
-	        	if(item.link === ""){
-	        		item.link = "#";
-	        	}
-	        	$min.append(" <li>\
-	        						<div>\
-	        							<a href= '"+item.link+"' class ='art-title' >"+item.title+"</a>\
-	        							<span class = 'art-date'>"+item.pubDate+"</span>\
-	        							<span>"+item.author+"</span>\
-	        							<p class = 'art-desc'>"+item.description+"</p>\
-	        						</div>\
-	        					</li>");
-	        	$min.find('div').addClass("article");
-	    	});
-
-			$('#ArticleModule').find("img").hide();
-    		// $min.find("p").hide();//hide description
-			$min.slideDown();
-		}else if (xhr.status >= 400){
-			console.log('There was an error!');
-		}
-
-	}
-	xhr.send(null);
+	     	} else {
+	       		xml = data;
+	     	}
+	     	var $xmlitems = $(xml).find("item");
+	    		$xmlitems.each(function() {
+		         var $this = $(this),
+		            item = {
+		                title: $this.find("title").text(),
+		                link:$this.find("link").text(),
+		                description: $this.find("description").text(),
+		                pubDate: $this.find("pubDate").text().substr(0,22),
+		                author: $this.find("author").text()
+		        	}
+		        	if(item.link === ""){
+		        		item.link = "#";
+		        	}
+		        	$min.append(" <li>\
+		        						<div>\
+		        							<a href= '"+item.link+"' class ='art-title' >"+item.title+"</a>\
+		        							<span class = 'art-date'>"+item.pubDate+"</span>\
+		        							<span>"+item.author+"</span>\
+		        							<div class = 'art-desc'>"+item.description+"</div>\
+		        						</div>\
+		        					</li>");
+		    	});
+				$min.find('li>div').addClass("article");
+		        $min.find('di').addClass("art-desc");
+				$('.progress').hide();
+				$min.slideDown();
+	   }
+	 });
 }
 function populateInitialRSS(){
 	var RSSFeeders = document.getElementById('RSSModule').getElementsByTagName('a');
-	var Feed = RSSFeeders[0];
+	var Feed = RSSFeeders[1];
 
 	//add selection highlight
 	$(Feed).addClass('selected');
 
 	//adds swirly vortex of waiting
 	var $title = $("#ArticleModule");
-	$title.append("<img src=../img/loader-bar.gif class=center>");
-	$title.find("img").show();
-
+	$title.append("<p class = 'progress' >Loading Articles...</p>");
+	$title.append("<img class = 'progress' src=../public/img/loader-bar.gif class=center>");
+	$('.progress').show();
 	getRSS(Feed);
 }
 
@@ -96,10 +109,10 @@ function getRSSWrapper(feed){
 	$(feed).addClass('selected'); //adds selection to current title
 
 	//adds swirly vortex of waiting
-	$('#ArticleModule').find("img").show();
+	$('.progress').show();
 
 	var $min = $("ul.article-list");
-	$min.html("");//clear all html already in
+	$min.html("");//clear all current articles
 	$min.slideUp();
-	getRSS(feed);
+	getRSS(feed);//get new feed
 }
